69c5704ef0c16ac3f2efe557bdc9c175336aae5b
Oskar Czekaj <oskar.czekaj@microsoft.wsei.edu.pl>
2024-11-14 19:30:31 +0100

    Dodaj pliki projektów.

---
+++ Controllers/DaneController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using obslugaformularzy.Models;
+
+public class DaneController : Controller
+{
+    [HttpGet]
+    public IActionResult Form()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Form(Dane dane)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("Wynik", dane);
+        }
+        return View();
+    }
+
+    public IActionResult Wynik(Dane dane)
+    {
+        return View(dane);
+    }
+}
---
+++ Models/Dane.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace obslugaformularzy.Models
+{
+    public class Dane
+    {
+        [Required(ErrorMessage = "Proszę podać Imię")]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać Email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}", ErrorMessage = "Nieprawidłowy adres email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać Temat")]
+        public string Temat { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać treść wiadomości")]
+        [MinLength(10, ErrorMessage = "Treść musi mieć co najmniej 10 znaków")]
+        [MaxLength(50, ErrorMessage = "Treść nie może przekraczać 50 znaków")]
+        public string Tresc { get; set; }
+    }
+}
